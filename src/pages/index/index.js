1e18e675ea44960fcb4c1517c7ceb197fa9cbb11
js-newbee <newbee.js@gmail.com>
2019-02-02 17:38:29 +0800

    refactor: 页面引入 redux 的方式

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,28 +1,13 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
-
-import { add, minus, asyncAdd } from '../../actions/counter'
-
+import * as actions from '@actions/counter'
 import './index.scss'
 
 
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
-}))
+@connect(state => state.counter, actions)
 class Index extends Component {
-
-    config = {
+  config = {
     navigationBarTitleText: '首页'
   }
 
@@ -42,7 +27,7 @@ class Index extends Component {
         <Button className='add_btn' onClick={this.props.add}>+</Button>
         <Button className='dec_btn' onClick={this.props.dec}>-</Button>
         <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
+        <View><Text>{this.props.num}</Text></View>
         <View><Text>Hello, World</Text></View>
       </View>
     )
