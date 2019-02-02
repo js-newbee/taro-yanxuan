92661b480957fbce5bc2f7e12f280a2681ded307
js-newbee <newbee.js@gmail.com>
2019-02-02 21:36:53 +0800

    upd: tabbar

--- src/app.js
+++ src/app.js
@@ -17,16 +17,45 @@ import './app.scss'
 const store = configStore()
 
 class App extends Component {
-
   config = {
     pages: [
-      'pages/index/index'
+      'pages/index/index',
+      'pages/cate/cate',
+      'pages/cart/cart',
+      'pages/user/user'
     ],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
+      navigationBarTitleText: '网易严选',
       navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+      color: "#666",
+      selectedColor: "#b4282d",
+      backgroundColor: "#fafafa",
+      borderStyle: 'black',
+      list: [{
+        pagePath: "pages/index/index",
+        iconPath: "./assets/tab-bar/index.png",
+        selectedIconPath: "./assets/tab-bar/index-active.png",
+        text: "首页"
+      }, {
+        pagePath: "pages/cate/cate",
+        iconPath: "./assets/tab-bar/cate.png",
+        selectedIconPath: "./assets/tab-bar/cate-active.png",
+        text: "分类"
+      }, {
+        pagePath: "pages/cart/cart",
+        iconPath: "./assets/tab-bar/cart.png",
+        selectedIconPath: "./assets/tab-bar/cart-active.png",
+        text: "购物车"
+      }, {
+        pagePath: "pages/user/user",
+        iconPath: "./assets/tab-bar/user.png",
+        selectedIconPath: "./assets/tab-bar/user-active.png",
+        text: "个人"
+      }]
     }
   }
 
