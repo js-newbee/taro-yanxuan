2cc5c285763951ff926da38ae94d348348c956cf
js-newbee <newbee.js@gmail.com>
2019-02-19 17:55:48 +0800

    docs: 完善 H5 跨域解决方案的注释

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
-// NOTE H5 端使用 devServer 实现跨域
+// NOTE H5 端使用 devServer 实现跨域，需要修改 package.json 的运行命令，加入环境变量
 const isH5 = process.env.CLIENT_ENV === 'h5'
 const HOST = '"https://miniapp.you.163.com"'
 const HOST_M = '"https://m.you.163.com"'
