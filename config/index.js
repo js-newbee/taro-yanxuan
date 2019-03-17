588267fe1d893e278b5b963e75f8adc619c8b9ba
js-newbee <newbee.js@gmail.com>
2019-03-17 21:27:42 +0800

    fix: sass importer 支持 ~(tilde) #16

--- config/index.js
+++ config/index.js
@@ -1,6 +1,13 @@
 const path = require('path')
 
-const sassImportor = function(url) {
+// NOTE 在 sass 中通过别名（@ 或 ~）引用需要指定路径
+const sassImporter = function(url) {
+  if (url[0] === '~' && url[1] !== '/') {
+    return {
+      file: path.resolve(__dirname, '..', 'node_modules', url.substr(1))
+    }
+  }
+
   const reg = /^@styles\/(.*)/
   return {
     file: reg.test(url) ? path.resolve(__dirname, '..', 'src/styles', url.match(reg)[1]) : url
@@ -33,7 +40,7 @@ const config = {
       ]
     },
     sass: {
-      importer: sassImportor
+      importer: sassImporter
     }
   },
   defineConstants: {
@@ -126,7 +133,7 @@ const config = {
       }
     },
     sassLoaderOption: {
-      importer: sassImportor
+      importer: sassImporter
     }
   }
 }
