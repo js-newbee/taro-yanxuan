293f939144616591e4f8515006f6c28aa33bfa35
js-newbee <newbee.js@gmail.com>
2019-03-28 23:49:35 +0800

    fix: 支付宝不支持 jsx  map 循环参数解构 #21

--- src/pages/home/recommend/index.js
+++ src/pages/home/recommend/index.js
@@ -23,52 +23,55 @@ export default class Recommend extends Component {
           <Text className='home-recommend__title-txt'>为你推荐</Text>
         </View>
         <View className='home-recommend__list'>
-          {list.filter(item => item.type === 1).map(({ id, categoryItem }) => (
-            <View
-              key={id}
-              className='home-recommend__list-item'
-              onClick={this.handleClick.bind(this, id)}
-            >
-              <Image className='home-recommend__list-item-img' src={categoryItem.listPicUrl} />
-              {!!categoryItem.simpleDesc && !categoryItem.simpleDescClose &&
-                <Text className='home-recommend__list-item-desc' numberOfLines={1}>
-                  {categoryItem.simpleDesc}
-                </Text>
-              }
-              <View className='home-recommend__list-item-info'>
-                {!!categoryItem.limitedTag &&
-                  <Tag text={categoryItem.limitedTag} />
-                }
-
-                <Text className='home-recommend__list-item-name' numberOfLines={1}>
-                  {categoryItem.name}
-                </Text>
-
-                <View className='home-recommend__list-item-price-wrap'>
-                  <Text className='home-recommend__list-item-price'>
-                    ¥{categoryItem.activityPrice || categoryItem.retailPrice}
+          {list.filter(item => item.type === 1).map((item) => {
+            const { id, categoryItem } = item
+            return (
+              <View
+                key={id}
+                className='home-recommend__list-item'
+                onClick={this.handleClick.bind(this, id)}
+              >
+                <Image className='home-recommend__list-item-img' src={categoryItem.listPicUrl} />
+                {!!categoryItem.simpleDesc && !categoryItem.simpleDescClose &&
+                  <Text className='home-recommend__list-item-desc' numberOfLines={1}>
+                    {categoryItem.simpleDesc}
                   </Text>
-                  {!!categoryItem.activityPrice &&
-                    <Text className='home-recommend__list-item-price--origin'>
-                      ¥{categoryItem.retailPrice}
+                }
+                <View className='home-recommend__list-item-info'>
+                  {!!categoryItem.limitedTag &&
+                    <Tag text={categoryItem.limitedTag} />
+                  }
+
+                  <Text className='home-recommend__list-item-name' numberOfLines={1}>
+                    {categoryItem.name}
+                  </Text>
+
+                  <View className='home-recommend__list-item-price-wrap'>
+                    <Text className='home-recommend__list-item-price'>
+                      ¥{categoryItem.activityPrice || categoryItem.retailPrice}
                     </Text>
+                    {!!categoryItem.activityPrice &&
+                      <Text className='home-recommend__list-item-price--origin'>
+                        ¥{categoryItem.retailPrice}
+                      </Text>
+                    }
+                  </View>
+
+                  {!!(categoryItem.comments && categoryItem.comments[0] && categoryItem.comments[0].content) &&
+                    <View className='home-recommend__list-item-commend'>
+                      <Image
+                        className='home-recommend__list-item-commend-img'
+                        src={categoryItem.comments[0].frontUserAvatar || defaultAvatar}
+                      />
+                      <Text className='home-recommend__list-item-commend-txt' numberOfLines={2}>
+                        {categoryItem.comments[0].content}
+                      </Text>
+                    </View>
                   }
                 </View>
-
-                {!!(categoryItem.comments && categoryItem.comments[0] && categoryItem.comments[0].content) &&
-                  <View className='home-recommend__list-item-commend'>
-                    <Image
-                      className='home-recommend__list-item-commend-img'
-                      src={categoryItem.comments[0].frontUserAvatar || defaultAvatar}
-                    />
-                    <Text className='home-recommend__list-item-commend-txt' numberOfLines={2}>
-                      {categoryItem.comments[0].content}
-                    </Text>
-                  </View>
-                }
               </View>
-            </View>
-          ))}
+            )
+          })}
         </View>
       </View>
     )
